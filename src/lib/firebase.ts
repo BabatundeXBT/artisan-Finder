eb2aafe28cc1f5cc621d5d1b9efccc960b44718f
Babatunde Zakariyau <babatundezakariyau27@gmail.com>
2025-08-25 15:55:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "artisan-direct",
-  appId: "1:246039021194:web:fe0a3b2ee02e406dd88f73",
-  storageBucket: "artisan-direct.firebasestorage.app",
-  apiKey: "AIzaSyBJNy9HhHaWD0xnsLzl9K8x-TY_e48ZtRg",
-  authDomain: "artisan-direct.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "246039021194"
+  "projectId": "artisan-direct",
+  "appId": "1:246039021194:web:fe0a3b2ee02e406dd88f73",
+  "storageBucket": "artisan-direct.firebasestorage.app",
+  "apiKey": "AIzaSyBJNy9HhHaWD0xnsLzl9K8x-TY_e48ZtRg",
+  "authDomain": "artisan-direct.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "246039021194"
 };
 
 // Initialize Firebase
